b3190016255b4f4b57be127dd237e34a600ce756
Hari hara prasad Viswanathan <harihvis@in.ibm.com>
2017-01-31 11:39:46 +0530

    added management action's test case

--- test/GatewayManagement.cs
+++ test/GatewayManagement.cs
@@ -12,6 +12,7 @@
 using System;
 using System.Collections.Generic;
 using NUnit.Framework;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace test
 {
@@ -24,6 +25,15 @@ namespace test
 		[SetUp]
 		public void Setup() 
 		{
+			Dictionary<string,string> data = IBMWIoTP.DeviceClient.parseFile("../../Resource/Gatewayprop.txt","## Gateway Registration detail");
+	    	if(	!data.TryGetValue("Organization-ID",out orgId)||
+	    		!data.TryGetValue("Device-Type",out deviceType)||
+	    		!data.TryGetValue("Device-ID",out deviceID)||
+	    		!data.TryGetValue("Authentication-Method",out authmethod)||
+	    		!data.TryGetValue("Authentication-Token",out authtoken) )
+	    	{
+	    		throw new Exception("Invalid property file");
+	    	}
 			testClient = new IBMWIoTP.GatewayManagement("../../Resource/Gatewayprop.txt",true);
 		}
 		[Test]
@@ -343,6 +353,111 @@ namespace test
 			Assert.IsTrue(wasCalled);
 			Assert.AreEqual(status,"200");
 			Assert.AreEqual(recivedId,uid);
+		}
+		//Action
+		[Test]
+		public void GatewayActionReboot()
+		{
+			bool wasCalled = false;
+			string recivedRequestId="",recivedAction="";
+			testClient.actionCallback += (string reqestId,string action)=>{
+				wasCalled = true;
+				recivedRequestId = reqestId;
+				recivedAction = action;
+			};
+			string msg = "{\"reqId\":\"b5f1b986-a8a4-4c92-aa11-97f532ee5947\"}";
+			string topic ="iotdm-1/type/"+deviceType+"/id/"+deviceID+"/mgmt/initiate/device/reboot";
+			MqttMsgPublishEventArgs evt = new MqttMsgPublishEventArgs(topic,System.Text.Encoding.UTF8.GetBytes(msg),false,1,true);
+			testClient.subscriptionHandler(new {},evt);
+			
+			
+			Assert.AreEqual(recivedRequestId ,"b5f1b986-a8a4-4c92-aa11-97f532ee5947");
+			Assert.AreEqual(recivedAction,"reboot");
+			Assert.IsTrue(wasCalled);
+		}
+	
+		[Test]
+		public void GatewayActionFactoryReset()
+		{
+			bool wasCalled = false;
+			string recivedRequestId="",recivedAction="";
+			testClient.actionCallback += (string reqestId,string action)=>{
+				wasCalled = true;
+				recivedRequestId = reqestId;
+				recivedAction = action;
+			};
+			string msg = "{\"reqId\":\"50f20f47-25a1-4f91-9bfb-b4267b1500b9\"}";
+			MqttMsgPublishEventArgs evt = new MqttMsgPublishEventArgs("iotdm-1/type/"+deviceType+"/id/"+deviceID+"/mgmt/initiate/device/factory_reset",System.Text.Encoding.UTF8.GetBytes(msg),false,1,true);
+			testClient.subscriptionHandler(new {},evt);
+			Assert.AreEqual(recivedRequestId ,"50f20f47-25a1-4f91-9bfb-b4267b1500b9");
+			Assert.AreEqual(recivedAction,"reset");
+			Assert.IsTrue(wasCalled);
+		}
+		[Test]
+		public void GatewayActionFirmeare_a_Download()
+		{
+			bool wasCalled = false;
+			string recivedAction="",uri="";
+			testClient.fwCallback += (string action, IBMWIoTP.DeviceFirmware fw)=>{
+				wasCalled = true;
+				recivedAction = action;
+				uri = fw.uri;
+				testClient.setState(IBMWIoTP.DeviceManagement.UPDATESTATE_DOWNLOADED);
+			};
+			string msgUpdate = "{\"reqId\":\"852536dd-db36-43c9-bed0-ba76124a1b90\",\"d\":{\"fields\":[{\"field\":\"mgmt.firmware\",\"value\":{\"version\":\"\",\"name\":\"\",\"uri\":\"https://test.com/file.dll\",\"verifier\":\"\",\"state\":0,\"updateStatus\":0,\"updatedDateTime\":\"\"}}],\"action\":\"firmware/download\"}}";
+			MqttMsgPublishEventArgs evtUpdate = new MqttMsgPublishEventArgs("iotdm-1/type/"+deviceType+"/id/"+deviceID+"/device/update",System.Text.Encoding.UTF8.GetBytes(msgUpdate),false,1,true);
+			testClient.subscriptionHandler(new {},evtUpdate);
+			
+			string msgObserve = "{\"reqId\":\"852536dd-db36-43c9-bed0-ba76124a1b90\",\"d\":{\"fields\":[{\"field\":\"mgmt.firmware\"}]}}";
+			MqttMsgPublishEventArgs evtObserve = new MqttMsgPublishEventArgs("iotdm-1/type/"+deviceType+"/id/"+deviceID+"/observe",System.Text.Encoding.UTF8.GetBytes(msgObserve),false,1,true);
+			testClient.subscriptionHandler(new {},evtObserve);
+			
+			
+			string msg = "{\"reqId\":\"852536dd-db36-43c9-bed0-ba76124a1b90\"}";
+			MqttMsgPublishEventArgs evt = new MqttMsgPublishEventArgs("iotdm-1/type/"+deviceType+"/id/"+deviceID+"/mgmt/initiate/firmware/download",System.Text.Encoding.UTF8.GetBytes(msg),false,1,true);
+			testClient.subscriptionHandler(new {},evt);
+			
+			string msgCancel = "{\"reqId\":\"852536dd-db36-43c9-bed0-ba76124a1b90\",\"d\":{\"fields\":[{\"field\":\"mgmt.firmware\"}]}}";
+			MqttMsgPublishEventArgs evtCancel = new MqttMsgPublishEventArgs("iotdm-1/type/"+deviceType+"/id/"+deviceID+"/cancel",System.Text.Encoding.UTF8.GetBytes(msgCancel),false,1,true);
+			testClient.subscriptionHandler(new {},evtCancel);
+			
+			Assert.AreEqual(recivedAction,"download");
+			Assert.AreEqual(uri,"https://test.com/file.dll");
+			Assert.IsTrue(wasCalled);
+		}
+		
+		[Test]
+		public void GatewayActionFirmeare_b_Update()
+		{
+			bool wasCalled = false;
+			string recivedAction="",uri="";
+			testClient.fwCallback += (string action, IBMWIoTP.DeviceFirmware fw)=>{
+				wasCalled = true;
+				recivedAction = action;
+				uri = fw.uri;
+				testClient.setUpdateState(IBMWIoTP.DeviceManagement.UPDATESTATE_SUCCESS);
+				
+			};
+			string msgUpdate = "{\"reqId\":\"980c9013-6e91-4d9c-898b-a9c4709a4708\",\"d\":{\"fields\":[{\"field\":\"mgmt.firmware\",\"value\":{\"version\":\"\",\"name\":\"\",\"uri\":\"https://test.com/file.dll\",\"verifier\":\"\",\"state\":0,\"updateStatus\":0,\"updatedDateTime\":\"\"}}],\"action\":\"firmware/update\"}}";
+			MqttMsgPublishEventArgs evtUpdate = new MqttMsgPublishEventArgs("iotdm-1/type/"+deviceType+"/id/"+deviceID+"/device/update",System.Text.Encoding.UTF8.GetBytes(msgUpdate),false,1,true);
+			testClient.subscriptionHandler(new {},evtUpdate);
+			
+			string msgObserve = "{\"reqId\":\"980c9013-6e91-4d9c-898b-a9c4709a4708\",\"d\":{\"fields\":[{\"field\":\"mgmt.firmware\"}]}}";
+			MqttMsgPublishEventArgs evtObserve = new MqttMsgPublishEventArgs("iotdm-1/type/"+deviceType+"/id/"+deviceID+"/observe",System.Text.Encoding.UTF8.GetBytes(msgObserve),false,1,true);
+			testClient.subscriptionHandler(new {},evtObserve);
+			
+			
+			string msg = "{\"reqId\":\"980c9013-6e91-4d9c-898b-a9c4709a4708\"}";
+			MqttMsgPublishEventArgs evt = new MqttMsgPublishEventArgs("iotdm-1/type/"+deviceType+"/id/"+deviceID+"/mgmt/initiate/firmware/update",System.Text.Encoding.UTF8.GetBytes(msg),false,1,true);
+			testClient.subscriptionHandler(new {},evt);
+			
+			string msgCancel = "{\"reqId\":\"980c9013-6e91-4d9c-898b-a9c4709a4708\",\"d\":{\"fields\":[{\"field\":\"mgmt.firmware\"}]}}";
+			MqttMsgPublishEventArgs evtCancel = new MqttMsgPublishEventArgs("iotdm-1/type/"+deviceType+"/id/"+deviceID+"/cancel",System.Text.Encoding.UTF8.GetBytes(msgCancel),false,1,true);
+			testClient.subscriptionHandler(new {},evtCancel);
+		
+			Assert.AreEqual(recivedAction,"update");
+			Assert.AreEqual(uri,"https://test.com/file.dll");
+			Assert.IsTrue(wasCalled);
 		}
 	}
 }
